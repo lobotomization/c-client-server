06a7d7d16e9e28e34814f2455773683f2333b275
Ian Kerr <lobotomization@gmail.com>
2016-10-11 02:23:56 -0400

    Added a broken implementation of statuses. Sending SIGUSR1 prints a table of statuses, but I can seem to get an array of strings to be properly shared between the children and parent. I'm going to replace it with an array of integers

--- A2P1.c
+++ A2P1.c
@@ -39,7 +39,7 @@ while(0)
 }\
 while(0)
 
-struct sockaddr_in getSockaddrFromPort(int port);
+
 struct prog_stats{
 	int size;
 	int compexitstatus;
@@ -55,7 +55,10 @@ struct prog_stats{
 	time_t replytime;
 	time_t deleted;
 };
-
+void status(int signal);
+struct sockaddr_in getSockaddrFromPort(int port); 
+void compileCode(char *sourcename, char *execname, int connection);
+void executeCode(char *execname, int connection);
 int spawnChildren(int num);
 int spawnChild();
 void handleConnection(int connection, int port);
@@ -68,12 +71,9 @@ void printMetadata(int fd, struct prog_stats *metadata);
 
 int main(int argc, char **argv){
 
-	//const int NUMCHLD = 10;
 	int pid = 0, index = 0; 
 	int shmfd;
 	struct prog_stats *childstatus = NULL;
-	pthread_mutex_t lock;
-	pthread_mutex_init(&lock, NULL);
 	/*void reaper(int signal){
 		wait(NULL);
 		children--;
@@ -83,55 +83,64 @@ int main(int argc, char **argv){
 		perror("Registering zombie reaper failed");
 		return EXIT_FAILURE;
 	}*/
+	int statusshmfd;												//initialize the statuses to zero
+	char **statuses;
+	initsharedmem("status", statusshmfd, char*, statuses, sizeof(char *)*NUMCHLD);
+	for(int i = 0; i < NUMCHLD; i++){
+		*(statuses + i) = (char *)malloc(100);
+	}
 
 
-	/*PIDshmfd = shm_open("PIDTable", O_RDWR|O_CREAT, 0666);
-	ftruncate(PIDshmfd, sizeof(int)*(NUMCHLD + 1));
-	PIDTable = (int *)mmap(0, sizeof(int)*(NUMCHLD + 1), PROT_READ | PROT_WRITE, MAP_SHARED, PIDshmfd, 0);
-	memset(PIDTable, 0, sizeof(int)*(NUMCHLD + 1));*/
-
+	if(SIG_ERR == signal(SIGUSR1, status))
+	{
+		perror("Registering status printer failed");
+		return EXIT_FAILURE;
+	}
 	int *PIDTable;
 	int PIDshmfd;
 	initsharedmem("PIDTable", PIDshmfd, int, PIDTable, NUMCHLD); //This line makes an area of shared memory whose name is "PIDTable", with type int and length NUMCHLD
 																	 //The pointer to shared memory is stored in the PIDTable variable
-	pthread_mutex_t *lockmem;
-	int lockshmfd;
-	initsharedmem("lock", lockshmfd, pthread_mutex_t, lockmem, 1);
-	*lockmem = lock; //This should set the shared memory to contain the address of the parent's mutex lock
+	struct flock pid_lock, meta_lock;
+
+	pid_lock.l_type = F_WRLCK;
+	pid_lock.l_whence = SEEK_SET;
+	pid_lock.l_start = 0;
+	pid_lock.l_len = sizeof(int)*(NUMCHLD + 1);
+
+	meta_lock.l_type = F_WRLCK;
+	meta_lock.l_whence = SEEK_SET;
+	meta_lock.l_start = 0;
+	meta_lock.l_len = sizeof(struct prog_stats);
+
 	spawnChildren(NUMCHLD);
 	while(1){
 		if(-1 == (pid = wait(NULL))){
 			perror("Child exited crappily");
 		}
+
+		opensharedmem("metadata", shmfd, struct prog_stats, childstatus, 1);
+		fcntl(shmfd, F_SETLKW, &meta_lock);
+
 		printf("Child exited with PID: %d\n", (int)pid);
 		for(index = 0; index < NUMCHLD; index++){
 			printf("PID[%d] = %d\n", index, *(PIDTable + index));
 		}
-		//while(*PIDTable == 1){
-		//	sleep(1);
-		//}
-		//if(*PIDTable == 0)
-		//*PIDTable = 1;
 
-		pthread_mutex_lock(&lock);
+
+		fcntl(PIDshmfd, F_SETLKW, &pid_lock);
 		for(index = 0; *(PIDTable + index) != (int)pid && index < NUMCHLD; index++); //Calculate index
 		printf("Deleting index %d\n", index); 
 		*(PIDTable + index) = 0;
-		//*PIDTable = 0;
-		//printf("Lock status: %d\n", *PIDTable);
+		pid_lock.l_type = F_UNLCK;
+		fcntl(PIDshmfd, F_SETLK, &pid_lock);
 
-		/*shmfd = shm_open("metadata", O_RDWR|O_CREAT, 0666);
-		ftruncate(shmfd, sizeof(struct prog_stats)); 
-		shm = mmap(0,sizeof(struct prog_stats), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
-		childstatus = (struct prog_stats *)shm;*/
-		opensharedmem("metadata", shmfd, struct prog_stats, childstatus, 1);
+
+		//opensharedmem("metadata", shmfd, struct prog_stats, childstatus, 1);
 		printMetadata(1, childstatus);
 		closesharedmem("metadata", shmfd, struct prog_stats, childstatus, 1);
-		pthread_mutex_unlock(&lock);
+		meta_lock.l_type = F_UNLCK;
+		fcntl(shmfd, F_SETLK, &meta_lock);
 
-		/*munmap(shm, sizeof(struct prog_stats));
-		shm_unlink("metadata");
-		close(shmfd);*/
 
 		spawnChildren(1);
 	}
@@ -161,24 +170,25 @@ int spawnChildren(int num){
 			f_lock.l_start = 0;
 			f_lock.l_len = sizeof(int)*(NUMCHLD + 1);
 
-			int PIDshmfd = shm_open("PIDTable", O_RDWR|O_CREAT, 0666), index = 0;
-			ftruncate(PIDshmfd, sizeof(int)*(NUMCHLD + 1));
-			int *PIDTable = (int *)mmap(0, sizeof(int)*(NUMCHLD + 1), PROT_READ | PROT_WRITE, MAP_SHARED, PIDshmfd, 0);
-			/*while(*PIDTable == 1){   //Be super careful about this, race conditions might happen here or in the next block!
-				sleep(1);
-			}
-			if(*PIDTable == 0){				///This is where proper locking needs to happen, race conditions can still happen here :(
-				*PIDTable = 1;*/
-				//pthread_mutex_lock(&lock);
-				fcntl(PIDshmfd, F_SETLKW, &f_lock);
-				for(index = 0; *(PIDTable + index) != 0 && index < NUMCHLD; index++); //Calculate index
-				printf("Now adding PID %d to index %d\n", (int)getpid(), index);
-				*(PIDTable + index) = (int)getpid();
-				f_lock.l_type = F_UNLCK;
-				fcntl(PIDshmfd, F_SETLK, &f_lock);
-				//pthread_mutex_unlock(&lock);
-				//*PIDTable = 0;
-				//}
+			int statusshmfd;
+			char **statuses;
+			opensharedmem("status", statusshmfd, char*, statuses, sizeof(char *)*NUMCHLD);
+
+			int *PIDTable;
+			int PIDshmfd , index;
+			opensharedmem("PIDTable", PIDshmfd, int, PIDTable, NUMCHLD);
+
+			fcntl(PIDshmfd, F_SETLKW, &f_lock);
+			for(index = 0; *(PIDTable + index) != 0 && index < NUMCHLD; index++); //Calculate index
+			printf("Now adding PID %d to index %d\n", (int)getpid(), index);
+			*(PIDTable + index) = (int)getpid();
+			f_lock.l_type = F_UNLCK;
+			fcntl(PIDshmfd, F_SETLK, &f_lock);
+			*(statuses + index) = (char *)malloc(100);
+			strcpy(*(statuses + index), "Initializing"); //Why the crap is this not accessible from the signal handler?
+			printf("*(statuses + %d) = %p\n", index, *(statuses + index));
+			printf("*(statuses + %d) points to the string %s\n", index, *(statuses + index));
+
 			spawnChild();
 			return (int)getpid();
 		}
@@ -194,13 +204,27 @@ int spawnChildren(int num){
 int spawnChild(){
 	const int INITPORT = 10000;
 	int listener, connection, port;
+
+	int statusshmfd;
+	char **statuses;
+	opensharedmem("status", statusshmfd, char*, statuses, sizeof(char *)*NUMCHLD);
+
+	int *PIDTable;
+	int PIDshmfd , index;
+	opensharedmem("PIDTable", PIDshmfd, int, PIDTable, NUMCHLD);
+	for(index = 0; *(PIDTable + index) != getpid() && index < NUMCHLD; index++);
+	
+
 	printf("Child process spawned...\n");
 	if(-1 == (port = listenOnPort(INITPORT, &listener))) //Port # is picked here
 	{
 		return EXIT_FAILURE;	
 	}
+	
+	//strcpy(*(statuses + index), "Listening\0"); 
 	connection = accept(listener, NULL, NULL);
 	printf("Child process connection now beginning...\n");
+	//strcpy(*(statuses + index), "Connecting\0"); 
 	if(0 > connection)
 	{
 		perror("Accepting connection failed, child exiting...");
@@ -219,31 +243,60 @@ int spawnChild(){
 	close(listener);
 	return EXIT_SUCCESS;
 }
+	void status(int signal){
+		int statusshmfd;
+		char **statuses;
+		opensharedmem("status", statusshmfd, char*, statuses, sizeof(char *)*NUMCHLD);
 
+		int *PIDTable;
+		int PIDshmfd;
+		opensharedmem("PIDTable", PIDshmfd, int, PIDTable, NUMCHLD);
+		printf("Index number\tPID\tStatus\n");
+		for(int i = 0; i < NUMCHLD; i++){
+			printf("%d\t\t%d\t%s\n", i, *(PIDTable + i), *(statuses+i));
+			//printf("*(statuses + %d) = %p\n", i, *(statuses + i));
+			//printf("*(statuses + %d) points to the string %s\n", i, *(statuses + i));
+		}
+
+	}
 void handleConnection(int connection, int port){
 
 
 
 	//long curTime = (long)time(NULL);
-	fcntl(connection, F_SETFD, FD_CLOEXEC);
+	//fcntl(connection, F_SETFD, FD_CLOEXEC);
+	int statusshmfd;
+	char **statuses;
+	opensharedmem("status", statusshmfd, char*, statuses, sizeof(char *)*NUMCHLD);
+
+	int *PIDTable;
+	int PIDshmfd , index;
+	opensharedmem("PIDTable", PIDshmfd, int, PIDTable, NUMCHLD);
+	for(index = 0; *(PIDTable + index) != getpid() && index < NUMCHLD; index++);
+	//strcpy(*(statuses + index), "Connected"); 
+
 	struct prog_stats *metadata = (struct prog_stats *)malloc(sizeof(struct prog_stats));
 	memset(metadata, 0, sizeof(struct prog_stats)); //Zero out the struct, in case the user exits immediately
+	
 	FILE *outFile = NULL;
-	int bytesRead, writingCode = 0, pid = 0, returnCode = 0, size = 0;
+	int bytesRead, writingCode = 0, returnCode = 0, size = 0;
 	char text[1024] = {0}, sourcename[100] = {0}, execname[100] = {0};
+
 	const char start[] = "start", end[] = "end", stop[] = "stop";
 	const char welcome[] = "Welcome to Ian's C auto-compiling server security nightmare!\nType \"%s\" to begin coding and \"%s\" to submit your code and \"%s\" to stop\n";
+	
 	dprintf(connection, welcome, start, end, stop);
 	bytesRead = read(connection, text, 1024);
 	while(-1 != bytesRead)
 	{
 
+		
 
 		if(0 == strncmp(text, stop, 4)){
 			if(1 == writingCode){
 				fclose(outFile);
 			}
-			return;
+			break;
 		}
 		if(1 == writingCode && 0 != strncmp(text, end, 3)){
 			fwrite(text, sizeof(char), bytesRead, outFile); 
@@ -251,49 +304,34 @@ void handleConnection(int connection, int port){
 		}
 
 		if(1 == writingCode && 0 == strncmp(text, end, 3)){
+			writingCode = 0;
 			fclose(outFile);
 			metadata->written = time(NULL);
 			write(connection, "Code written to file\n", sizeof("Code written to file\n"));
 			strncpy(execname, sourcename, strlen(sourcename));
+			*(execname + strlen(execname) - 2) = '\0'; //Chop off .c extension
 			metadata->size = size;
 			metadata->sourcename = sourcename;
-			metadata->execname = execname;			
-			char *argv[10] = {0};
-			argv[0] = "gcc";
-			argv[1] = sourcename;
-			printf("Source code filename is: %s\n", sourcename);
-			argv[2] = "-o";
-			*(execname + strlen(execname) - 2) = '\0'; //Chop off .c extension
-			argv[3] = execname;
-			printf("Executable filename is: %s\n", execname);
-			argv[4] = NULL;
-			writingCode = 0;
-			write(connection, "Now compiling code...\n", sizeof("Now compiling code...\n"));
-			pid = fork();
-			if(pid == 0){
-				dup2(connection, STDOUT_FILENO);//Redirect stderr and stdout to the client's connection
-				dup2(connection, STDERR_FILENO);
-				execvp(argv[0], argv); 		//Run gcc as a child of the child
-			}
+			metadata->execname = execname;	
+
+			compileCode(sourcename, execname, connection);
+
 			wait(&returnCode);
 			metadata->compiled = time(NULL);
 			metadata->compexitstatus = returnCode;
-			dprintf(connection, "Your return code is: %d\n", returnCode);
-			pid = fork();
+			dprintf(connection, "Your return code is: %d\n", returnCode);	
 			metadata->started = time(NULL);
-			if(returnCode == 0 && pid == 0){
-				dup2(connection, STDOUT_FILENO);//Redirect stderr and stdout to the client's connection
-				dup2(connection, STDERR_FILENO);
-				argv[0] = execname;
-				argv[1] = NULL;
-				write(connection, "Now executing code...\n", sizeof("Now executing code...\n"));
-				execvp(argv[0], argv); //Requires . to be in PATH variable to work
-				//This executes the compiled code
+			if(returnCode == 0){
+				executeCode(execname, connection);
+
 			}
 			wait(&returnCode);
+			
+
 			metadata->finished = time(NULL);
 			metadata->runtime = metadata->finished - metadata->started;
 			metadata->progexitstatus = returnCode;
+
 			unlink(sourcename);
 			unlink(execname);
 			metadata->deleted = time(NULL);
@@ -305,9 +343,18 @@ void handleConnection(int connection, int port){
 				dprintf(connection, "Compilation failed\n");
 			}
 			metadata->replytime = time(NULL);
+
+			struct flock meta_lock;
+			meta_lock.l_type = F_WRLCK;
+			meta_lock.l_whence = SEEK_SET;
+			meta_lock.l_start = 0;
+			meta_lock.l_len = sizeof(struct prog_stats);
+
 			int shmfd;
 			struct prog_stats *shmprogstats;
 			opensharedmem("metadata", shmfd, struct prog_stats, shmprogstats, 1);
+			fcntl(shmfd, F_SETLKW, &meta_lock);
+
 			/*
 			int shmfd = shm_open("metadata", O_RDWR|O_CREAT, 0666);
 			ftruncate(shmfd, sizeof(struct prog_stats)); //I should figure out a better bound than just 4KB for no good reason
@@ -320,8 +367,10 @@ void handleConnection(int connection, int port){
 			strncpy(shmprogstats -> sourcename, metadata->sourcename, strlen(metadata->sourcename) + 1); 
 			//These strncpy and malloc lines make sure that shm has a copy of the data being pointed to
 			//Rather than just a copy of the pointers
-			shmprogstats -> execname = malloc(strlen(metadata->execname));
+			shmprogstats -> execname = (char *)malloc(strlen(metadata->execname));
 			strncpy(shmprogstats -> execname, metadata->execname, strlen(metadata->execname) + 1);
+			meta_lock.l_type = F_UNLCK;
+			fcntl(shmfd, F_SETLK, &meta_lock);
 			printMetadata(connection, shmprogstats);
 
 			//munmap(shm, sizeof(struct prog_stats));
@@ -343,10 +392,40 @@ void handleConnection(int connection, int port){
 				metadata->received = time(NULL);
 			}
 		}
-		bytesRead = read(connection, text, 1024);
+	bytesRead = read(connection, text, 1024);	
+
 	}
 }
 
+void executeCode(char *execname, int connection){
+	char *argv[2] = {execname, NULL};
+	write(connection, "Now executing code...\n", sizeof("Now executing code...\n"));
+	int pid = fork();
+	if(pid == 0){
+		dup2(connection, STDOUT_FILENO);//Redirect stderr and stdout to the client's connection
+		dup2(connection, STDERR_FILENO);
+		execvp(argv[0], argv); //Requires . to be in PATH variable to work
+		//This executes the compiled code
+	}
+}
+
+void compileCode(char *sourcename, char *execname, int connection){
+	char *argv[5] = {"gcc", sourcename, "-o", execname, NULL};
+	//argv[0] = "gcc";
+	//argv[1] = sourcename;
+	printf("Source code filename is: %s\n", sourcename);
+	//argv[2] = "-o";
+	//argv[3] = execname;
+	printf("Executable filename is: %s\n", execname);
+	//argv[4] = NULL;
+	write(connection, "Now compiling code...\n", sizeof("Now compiling code...\n"));
+	int pid = fork();
+	if(pid == 0){
+		dup2(connection, STDOUT_FILENO);//Redirect stderr and stdout to the client's connection
+		dup2(connection, STDERR_FILENO);
+		execvp(argv[0], argv); 		//Run gcc as a child of the child
+	}
+}
 struct sockaddr_in getSockaddrFromPort(int port){
 	struct sockaddr_in out;
 	memset(&out, 0, sizeof(out));
